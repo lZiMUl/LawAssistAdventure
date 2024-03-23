8862f74d9829d7ebc50b082eff1bdf4507e12752
lZiMUl <AA2908554069@gmail.com>
2024-03-23 08:00:00 -0700

    Mod Project File Update

--- src/main/java/com/lzimul/LawAssistAdventure/client/hud/GunHud.java
+++ src/main/java/com/lzimul/LawAssistAdventure/client/hud/GunHud.java
@@ -14,19 +14,13 @@ import static com.lzimul.LawAssistAdventure.client.util.ConfigUtil.getHudResourc
 
 public class GunHud implements IGuiOverlay {
     private final Minecraft minecraft;
-    private final int screenWidth;
-    private final int screemHeight;
     private final int imageWidth;
     private final int imageHeight;
 
     public GunHud() {
         this.minecraft = Minecraft.getInstance();
-        this.screenWidth = Minecraft.getInstance().getWindow().getScreenWidth();
-        this.screemHeight = Minecraft.getInstance().getWindow().getScreenHeight();
-        this.imageWidth = screenWidth - 1080;
-        this.imageHeight = screemHeight - 607;
-        assert Minecraft.getInstance().level != null;
-        Objects.requireNonNull(Minecraft.getInstance().level.players().get(0)).sendSystemMessage(Component.literal("[" + screenWidth + ", " + screemHeight + "] | [" + imageWidth + "," + imageHeight + "]"));
+        this.imageWidth = 1080 / 10;
+        this.imageHeight = 607 / 10;
     }
 
     public static GunHud getInstance() {
@@ -37,6 +31,8 @@ public class GunHud implements IGuiOverlay {
     public void render(@NotNull ExtendedGui extendedGui, @NotNull GuiGraphics guiGraphics, float partialTick, int screenWidth, int screenHeight) {
         assert minecraft.player != null;
         if (minecraft.player.getMainHandItem().getItem() == ItemRegister.Glock19.get().asItem()) {
+            assert Minecraft.getInstance().level != null;
+            Minecraft.getInstance().level.players().get(0).sendSystemMessage(Component.literal("[" + screenWidth + ", " + screenHeight + "] | [" + imageWidth + "," + imageHeight + "]"));
             guiGraphics.setColor(1, 1, 1, 1);
             guiGraphics.blit(getHudResourceLocation("gun_hud"), screenWidth - imageWidth, screenHeight - imageHeight, 0, 0, screenWidth - 10, screenHeight - 10, imageWidth, imageHeight);
         }
