01d5d87fe98f6a98416dc22ff188b6f1029b4c1e
lZiMUl <AA2908554069@gmail.com>
2024-03-28 18:08:19 -0700

    Update Sounds File

--- src/main/java/com/lzimul/LawAssistAdventure/network/ClientProviderNetwork.java
+++ src/main/java/com/lzimul/LawAssistAdventure/network/ClientProviderNetwork.java
@@ -1,6 +1,7 @@
 package com.lzimul.LawAssistAdventure.network;
 
 
+import net.minecraft.network.chat.Component;
 import net.neoforged.neoforge.network.handling.PlayPayloadContext;
 
 public class ClientProviderNetwork {
@@ -12,5 +13,9 @@ public class ClientProviderNetwork {
     }
 
     public void handleData(final DataNetwork networkData, final PlayPayloadContext playPayloadContext) {
+        playPayloadContext.player().ifPresent((player) -> playPayloadContext.workHandler().submitAsync(() -> {
+            String data = networkData.data();
+            player.sendSystemMessage(Component.literal("我从客户端得到了这个数据:" + data));
+        }));
     }
 }
