8b19f0cc672de7dc2ae4daf416e8c01a4e12eed9
lZiMUl <AA2908554069@gmail.com>
2024-04-20 21:34:15 -0700

    Added KeyBinding

--- src/main/java/com/lzimul/LawAssistAdventure/item/Ak47Item.java
+++ src/main/java/com/lzimul/LawAssistAdventure/item/Ak47Item.java
@@ -4,21 +4,30 @@ import com.lzimul.LawAssistAdventure.client.helper.AmmunitionHelper;
 import com.lzimul.LawAssistAdventure.register.SoundRegister;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Vec3i;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.List;
+
+import static com.lzimul.LawAssistAdventure.Config.*;
 
 public class Ak47Item extends Item {
     public static final AmmunitionHelper ammunitionHelper = new AmmunitionHelper(30, 270, 0);
-
+    private static final List<Block> targetBlocks = Arrays.stream(new Block[]{Blocks.GLASS, Blocks.GLASS_PANE, Blocks.OAK_LOG, Blocks.STONE}).distinct().toList();
     public Ak47Item() {
         super(new Item.Properties().stacksTo(1));
     }
@@ -44,6 +53,12 @@ public class Ak47Item extends Item {
             if (ammunitionHelper.getCurrent() != 0) {
                 ammunitionHelper.fire(1);
                 level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundRegister.AK47Fire.get(), player.getSoundSource(), 1.0F, 1.0F);
+                for (Vec3 point : getRay(player)) {
+                    Entity hitEntity = getEntityAtPoint(player, point);
+                    if (hitEntity != null && isAir(level, new BlockPos(new Vec3i((int) point.x, (int) point.y, (int) point.z)), targetBlocks)) {
+                        player.attack(hitEntity);
+                    }
+                }
             } else {
                 player.sendSystemMessage(Component.translatable("event.law_assist_adventure.ammunition.low"));
             }
