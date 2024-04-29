4c04d58e0251093da8016c51dece6b0c3dcffd9c
lZiMUl <AA2908554069@gmail.com>
2024-04-29 08:39:14 -0700

    Minecraft 1.20.5 is supported, but there are some bugs

--- src/main/java/com/lzimul/LawAssistAdventure/block/IndustrialSmeltingFurnacesBlock.java
+++ src/main/java/com/lzimul/LawAssistAdventure/block/IndustrialSmeltingFurnacesBlock.java
@@ -11,6 +11,7 @@ public class IndustrialSmeltingFurnacesBlock extends Block {
 //    @Override
 //    public @NotNull InteractionResult use(@NotNull BlockState blockState, @NotNull Level level, @NotNull BlockPos blockPos, @NotNull Player player, @NotNull InteractionHand interactionHand, @NotNull BlockHitResult blockHitResult) {
     // TODO 待开发点击工业冶炼炉显示界面
+
 //        player.sendSystemMessage(Component.literal(IndustrialSmeltingFurnacesBlock.class.getName()));
 //        return super.use(blockState, level, blockPos, player, interactionHand, blockHitResult);
 //    }
